f7a062aba01102b524a875c5b1b84b94176e3084
Su-daa <subin.kk019@gmail.com>
2023-10-12 14:33:11 +0900

    1010 문제풀이

---
+++ src/fc1010/bj9253.java
@@ -0,0 +1,26 @@
+package fc1010;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//스페셜 져지
+public class bj9253 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        String answer = br.readLine();
+
+//        char[] s1 = str1.toCharArray();
+//        char[] s2 = str2.toCharArray();
+//        char[] ans = answer.toCharArray();
+
+        if (str1.contains(answer) && str2.contains(answer)) {
+            System.out.println("YES");
+        }else {
+            System.out.println("NO");
+        }
+
+    }
+}
