e312d74bed3a408e1b0f85e824d9649da4320581
Su-daa <subin.kk019@gmail.com>
2023-12-08 13:21:16 +0900

    1207 문제풀이

---
+++ src/fc1205/pg181851.java
@@ -0,0 +1,33 @@
+package fc1205;
+
+//전국 대회 선발 고사
+public class pg181851 {
+    public static void main(String[] args) {
+
+//        int[][] array = new int[rank.length][2];
+//        for (int i = 0; i < rank.length; i++) {
+//            array[i][0] = rank[i];
+//            array[i][1] = i;
+//        }
+//
+//        Arrays.sort(array, (a, b) -> a[0] - b[0]);
+//
+//        int[] students = new int[3];
+//        int index = 0;
+//        int count = 0;
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if (attendance[array[i][1]]) {
+//                students[index++] = array[i][1];
+//                count++;
+//            }
+//            if (count == 3) {
+//                break;
+//            }
+//        }
+//
+//        int answer = 10000 * students[0] + 100 * students[1] + students[2];
+//
+//        return answer;
+    }
+}
--- src/fc1207/pg120896.java
+++ src/fc1207/pg120896.java
@@ -1,29 +1,39 @@
 package fc1207;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 //한 번만 등장한 문자
 public class pg120896 {
     public static void main(String[] args) {
         String s = "abcabcadc";
-        List<String> answer = new ArrayList<>();
-        String[] split = s.split("");
-        Arrays.sort(split);
-
-        for (int i = 0; i < split.length; i++) {
-            answer.add(split[i]);
-
-            if (!answer.contains(String.valueOf(split[i]))) {
+//
+//        List<String> answer = new ArrayList<>();
+//        String[] split = s.split("");
+//        Arrays.sort(split);
+//
+//        for (int i = 0; i < split.length; i++) {
+//            answer.add(split[i]);
+//
+//            if (!answer.contains(String.valueOf(split[i]))) {
+//            }
+//        }
+//        answer.sort(Comparator.naturalOrder());
+//        StringBuilder sb = new StringBuilder();
+//        for (String c : answer) {
+//            sb.append(c);
+//        }
+//        System.out.println(sb.toString());
+        StringBuilder answer = new StringBuilder();
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for (Character c : map.keySet()) {
+            if (map.get(c)==1){
+                answer.append(c);
             }
         }
-        answer.sort(Comparator.naturalOrder());
-        StringBuilder sb = new StringBuilder();
-        for (String c : answer) {
-            sb.append(c);
-        }
-        System.out.println(sb.toString());
+
     }
 }
