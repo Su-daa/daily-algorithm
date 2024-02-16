6c60241de7a5dc17c198b6ee72809f24098121b4
Su-daa <subin.kk019@gmail.com>
2024-02-16 15:24:55 +0900

    0206 문제풀이

--- src/daily0206/bj2606.java
+++ src/daily0206/bj2606.java
@@ -6,27 +6,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class bj2606 {
-    public static int N,M;
+    public static int N,M,ans;
     public static boolean[] isVisited;
     public static int[][] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
         isVisited = new boolean[N + 1];
         arr = new int[N + 1][N + 1];
+
         for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             arr[a][b] = arr[b][a] = 1;
         }
+        dfs(1);
+        System.out.println(ans);
     }
 
     public static void dfs(int start) {
         isVisited[start] = true;
-        
+        for (int i = 1; i <= N; i++) {
+            if (arr[start][i] == 1 && !isVisited[i]) {
+                ans++;
+                dfs(i);
+            }
+        }
     }
 }
