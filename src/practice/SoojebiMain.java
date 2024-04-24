b70fca89e7f3c2b10fe268750e78c323ee0ff85a
Su-daa <subin.kk019@gmail.com>
2024-04-24 22:59:10 +0900

    0424 정처기

---
+++ src/practice/SoojebiMain.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class SoojebiMain {
+    public static void main(String[] args) {
+        Soojebi.print();
+    }
+}
+
+class Soojebi{
+    public static void print() {
+        System.out.println("soojebi class");
+    }
+}
