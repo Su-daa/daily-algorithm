ac705bd24c9e17a1749f34db3bf8a015acfb0b8a
Su-daa <subin.kk019@gmail.com>
2024-10-07 23:23:22 +0900

    [1007] Chap06

--- src/programmers/p_76502.java
+++ src/programmers/p_76502.java
@@ -1,8 +1,41 @@
 package programmers;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 public class p_76502 {
-    public int solution(String s) {
-        int answer = -1;
+    public static void main(String[] args) {
+        System.out.println(solution("}}}"));
+        System.out.println(solution("{[]()}"));
+    }
+    public static int solution(String s) {
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        int n = s.length();
+        s += s;
+        int answer = 0;
+
+        A: for (int i = 0; i < n; i++) {
+            Stack<Character> stack = new Stack<>();
+            for (int j = i; j < i + n; j++) {
+                char c = s.charAt(j);
+                if (!map.containsKey(c)) {
+                    stack.push(c);
+                }
+                else {
+                    if (stack.isEmpty() || !stack.pop().equals(map.get(c))) {
+                        continue A;
+                    }
+                }
+            }
+            if (stack.isEmpty()) {
+                answer++;
+            }
+        }
+
         return answer;
     }
 }
